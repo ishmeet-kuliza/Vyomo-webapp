19662590ecc25e66181bbcc14b4172d766e627d2
Eklove <eklove.walia@kuliza.com>
2016-04-14 20:02:49 +0530

    promo code api integration

--- src/common/directives/totalPriceHeader.js
+++ src/common/directives/totalPriceHeader.js
@@ -1,4 +1,4 @@
-angular.module('Vyomo').directive('totalPriceHeader' , ['cart' , function(cart){
+angular.module('Vyomo').directive('totalPriceHeader' , ['cart' ,'promoCodeService', function(cart, promoCodeService){
     return {
         restrict : 'AE',
         scope:{
@@ -7,24 +7,41 @@ angular.module('Vyomo').directive('totalPriceHeader' , ['cart' , function(cart){
             discount : '@'
         },
         template : '<div class="vm-cart-bill-row"><div class="vm-offer-code-box list-inline-elems">'+
-                    '<input type="text" name="offerCode" value="{{offerCode}}" class="form-control" placeholder="Enter Offer Code"></div><div class="list-inline-elems pull-right totalField fields">'+
+                    '<input type="text" name="offerCode" ng-model="promoCode" class="form-control" placeholder="Enter Offer Code"></div><div class="list-inline-elems pull-right totalField fields">'+
                     'Total Cost<span class="totalVal font-bold">&#8377 {{totalPrice}}</span></div>'+
                     '<div class="list-inline-elems pull-right disountField fields">Discount'+
                     '<span class="discountVal font-bold">&#8377 {{discount}}</span></div>'+
                     '<div class="list-inline-elems pull-right subTotalField fields">Sub Total'+
-                    '<span class="subTotalVal font-bold">&#8377 {{subTotal}}</span></div></div>',
+                    '<span class="subTotalVal font-bold">&#8377 {{subTotal}}</span></div>'+
+                    '<span><button class="btn" ng-click="verifyPromoCode()"></span></div>',
+
+        // templateUrl: '/directives/templates/total-price-header.html',
 
         replace : true,
 
         link: function($scope){
             $scope.$watchCollection(function(){
-            return cart.totalPrice;
-        }, 
-        function(newPrice){
-            $scope.totalPrice = newPrice;
-        });
-        }
+                return cart.totalPrice;
+            },
+            function(newPrice){
+                $scope.totalPrice = newPrice;
+            });
 
-};
+            $scope.promoCode = null;
+            $scope.discount = 0;
+            $scope.errorMsg = '';
+            $scope.verifyPromoCode = function(){
+                if($scope.promoCode && $scope.promoCode.length){
+                    promoCodeService.verifyCode($scope.promoCode).then(function(costAfterPromo){
+                        $scope.discount = cart.totalPrice - costAfterPromo;
+                        cart.totalPrice = costAfterPromo;
+                    },function(error){
+                        $scope.errorMsg = error;
+                        window.console.log(error);
+                    });
+                }
+            };
+        }
+    };
 
 }]);
--- src/common/services/promoCodeService.js
+++ src/common/services/promoCodeService.js
@@ -1,26 +1,36 @@
 /**
- *  Services/ Factory for add and get address
+ *  Services/ Factory for promo code
  *  @author/ Ekluv-Dev
  */
 
  angular.module('Vyomo')
-  .factory('promoCode', ['$http', 'cart', 'auth', 'env', function($http, cart, auth, env){
+  .factory('promoCodeService', ['$http', 'cart', 'auth', 'env', '$q', function($http, cart, auth, env, $q){
         var promoCode = {};
         var BASE_URL = env.BASE_URL;
-        promoCode.verify_code = function(code){
-            var accessToken = env.getUser().sessionToken;
+        promoCode.verifyCode = function(code){
+            var accessToken = auth.getUser().sessionToken;
             var postData = {
-                service_ids: cart.getItems(),
+                service_ids: cart.getAllItems().toString(),
                 access_token: accessToken,
                 promotional_code: code,
-                when: ''
+                when: '2016-04-15 13:00'
             };
-
-            return $http({
-                method: 'POST',
-                data: postData,
-                url: BASE_URL + '/verify_promotional_code'
+            var url = BASE_URL + '/verify_promotional_code';
+            var deferred = $q.defer();
+            $http.post(url, postData).then(function(response){
+                if(response && response.data && response.data.status_code === 200){
+                    if(response.data.message){
+                        var costAfterPromo = response.data.message.cost_after_promo;
+                        deferred.resolve(costAfterPromo);
+                    }
+                }
+                else{
+                    deferred.reject(response.data.error_message);
+                }
+            },function(error){
+                deferred.reject(error);
             });
+            return deferred.promise;
         };
   return promoCode;
 }]);
