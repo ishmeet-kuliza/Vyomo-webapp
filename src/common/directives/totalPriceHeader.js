05efcfe51f0da100bce6c3142a26c6d48fa52178
Gaurav Pandvia <gaurav.pandvia@minjar.com>
2016-04-16 20:26:51 +0530

    Small changes cart js

--- src/common/directives/totalPriceHeader.js
+++ src/common/directives/totalPriceHeader.js
@@ -4,16 +4,19 @@ angular.module('Vyomo').directive('totalPriceHeader' , ['cart' ,'promoCodeServic
         scope:{
             offerCode : '@',
             subTotal : '@',
-            discount : '@'
+            discount : '@',
+            showOfferCode : '@'
         },
-        template : '<div class="vm-cart-bill-row"><div class="vm-offer-code-box list-inline-elems">'+
-                    '<input type="text" name="offerCode" ng-model="promoCode" class="form-control" placeholder="Enter Offer Code"></div><div class="list-inline-elems pull-right totalField fields">'+
+        template : '<div class="vm-cart-bill-row"><div class="vm-offer-code-box list-inline-elems" ng-if="showOfferCode">'+
+                    '<input type="text" name="offerCode" ng-model="promoCode" class="form-control" placeholder="Enter Offer Code">' +
+                    '<span class="success-control-label"></span><span class="control-label"></span></div>' +
+                    '<span class="vm-link ml-10 mt-10" id="vm-applyCoupon" ng-if="showOfferCode">Apply</span>' +
+                    '<div class="list-inline-elems pull-right totalField fields mt-10">'+
                     'Total Cost<span class="totalVal font-bold">&#8377 {{totalPrice}}</span></div>'+
-                    '<div class="list-inline-elems pull-right disountField fields">Discount'+
+                    '<div class="list-inline-elems pull-right disountField fields mt-10">Discount'+
                     '<span class="discountVal font-bold">&#8377 {{discount}}</span></div>'+
-                    '<div class="list-inline-elems pull-right subTotalField fields">Sub Total'+
-                    '<span class="subTotalVal font-bold">&#8377 {{subTotal}}</span></div>'+
-                    '<span><button class="btn" ng-click="verifyPromoCode()"></span></div>',
+                    '<div class="list-inline-elems pull-right subTotalField fields mt-10">Sub Total'+
+                    '<span class="subTotalVal font-bold">&#8377 {{subTotal}}</span></div>',
 
         // templateUrl: '/directives/templates/total-price-header.html',
 
